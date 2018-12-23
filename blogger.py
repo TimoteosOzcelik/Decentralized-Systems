edd6847b7d30f04755e983f1892ec65067217958
TimoteosOzcelik <timoteos.ozcelik@gmail.com>
2018-12-23 17:28:11 +0300

    Update

--- blogger.py
+++ blogger.py
@@ -1,11 +1,16 @@
 import threading
 import socket
+import sys
+import os
+import uuid
+from Crypto.PublicKey import RSA
+from Crypto import Random
 import queue
 import logging
 
 
 class Connection(threading.Thread):
-    def __init__(self):
+    def __init__(self, host, port):
         threading.Thread.__init__(self)
 
     def run(self):
@@ -21,74 +26,156 @@ class Server(threading.Thread):
 
 
 class Client(threading.Thread):
-    def __init__(self, host, port, cs_info):
+    def __init__(self, server_uuid, server_host, server_port, server_private ,host, port, cs_info, type):
         threading.Thread.__init__(self)
+        # Blogger
+        self.is_blogger = 'Y'
+        # Its server information to share
+        self.server_uuid = server_uuid
+        self.server_host = server_host
+        self.server_port = server_port
+        self.server_private = server_private
+        # TODO: Get nickname from interface
+        self.nickname = ''
+        # Other side server information to connect & check
         self.host = host
         self.port = port
+        self.public_key = ''
+        # Client - Server Information Queue
         self.cs_info = cs_info
+        # Socket
         self.sock = socket.socket()
+        # False response count
+        self.error = 0
+        # Request or Connect
+        self.type = type
 
     def run(self):
         self.connect()
-        self.demand_public_key()
+        if self.type == 'Request':
+            pass
+        else:
+            pass
+        # self.demand_public_key()
 
+    # Request & Response
     def connect(self):
         self.sock.connect((self.host, self.port))
 
+    # Request & Response
     def disconnect(self):
         self.sock.close()
+        sys.exit()
 
+    # Request
+    def login(self):
+        req = 'INF'
+        self.sock.send(
+            (req + self.server_uuid + ';' + self.server_host + ';' + self.server_port + ';' + self.is_blogger
+             + ';' + self.nickname).encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+        '''
+        if resp == 'HEL':
+            # TODO: Add to peer table as connected --> TO
+            pass
+        '''
+
+    # Request
+    def demand_peer_list(self):
+        req = 'LSQ'
+        self.sock.send(req.encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+
+    # Request & Response
+    def demand_public_key(self):
+        req = 'PUB'
+        self.sock.send(req.encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+
+    # Request & Response
+    def demand_signed_hash(self, public_key):
+        req = 'SMS'
+        self.sock.send(req.encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+
+    # Request
+    def subscribe(self):
+        req = 'SUB'
+        self.sock.send(self.public_key.encrypt(req.encode(), 32).encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+
+    # Request
+    def unsubscribe(self):
+        req = 'USB'
+        self.sock.send(self.public_key.encrypt(req.encode(), 32).encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+
+    # Request
+    def demand_microblog(self):
+        pass
+
+    # Response
     def check_identity(self):
         pass
 
+    # Request & Response
     def check_connection(self):
+        req = 'TIC'
+        self.sock.send(req.encode())
+        resp = self.sock.recv(1024).decode()
+        self.parser(req, resp)
+        # TODO: Update timestamp
+
+    # Request
+    def send_message(self, message):
         pass
 
-    def demand_public_key(self):
-        try:
-            self.sock.send('PUB'.encode())
-            resp = self.sock.recv(1024).decode()
-            public_key = self.parser(resp)
-            check = self.demand_signed_hash(public_key)
-            if check:
-                pass
-            else:
-                pass
-        except:
-            self.disconnect()
-
-    def demand_signed_hash(self, public_key):
-        try:
-            self.sock.send('SMS'.encode())
-            resp = self.sock.recv(1024).decode()
-            check = self.parser(resp)
-            return check
-        except:
-            self.disconnect()
-
-    def send_message(self, message):
-        try:
-            self.sock.send(message.encode())
-            resp = self.sock.recv(1024).decode()
-            parsed = self.parser(resp)
-        except:
-            self.disconnect()
-
+    # Response
     def blocked(self):
-        try:
-            self.sock.send('SBM'.encode())
-            resp = self.sock.recv(1024).decode()
-            parsed = self.parser(resp)
-        except:
-            self.disconnect()
+        pass
 
+    # Response
     def unblocked(self):
-        try:
-            self.sock.send('SUM'.encode())
-            resp = self.sock.recv(1024).decode()
-            parsed = self.parser(resp)
-        except:
-            self.disconnect()
+        pass
 
-    def parser(self, rec):
-        return rec
+    # Request & Response
+    def parser(self, request, received):
+        pass
+
+
+def main():
+    exists_pem = os.path.isfile('id_rsa.pem')
+    exists_pub = os.path.isfile('id_rsa.pub')
+    exists_uuid = os.path.isfile('uuid.pem')
+    if exists_pem and exists_pub and exists_uuid:
+        blogger_public_key = RSA.importKey(open('id_rsa.pem', 'rb').read())
+        blogger_private_key = RSA.importKey(open('id_rsa.pub', 'rb').read())
+        blogger_uuid = uuid.UUID(open('uuid.pem', 'r').read())
+    else:
+        random_generator = Random.new().read
+        new_key = RSA.generate(2048, randfunc=random_generator)
+        blogger_public_key = new_key.publickey()
+        blogger_private_key = new_key
+        blogger_uuid = uuid.uuid4()
+
+        f = open('id_rsa.pem', 'w')
+        f.write(blogger_private_key.exportKey().decode())
+        f.close()
+
+        f = open('id_rsa.pub', 'w')
+        f.write(blogger_public_key.exportKey().decode())
+        f.close()
+
+        f = open('uuid.pem', 'w')
+        f.write(blogger_uuid.__str__())
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
