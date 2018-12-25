d9d53686b6429c52d7de9be05e6c830f427357a1
TimoteosOzcelik <timoteos.ozcelik@gmail.com>
2018-12-25 21:15:48 +0300

    Blogger Server Login

--- blogger.py
+++ blogger.py
@@ -18,49 +18,81 @@ class Connection(threading.Thread):
 
 
 class Server(threading.Thread):
-    def __init__(self):
+    def __init__(self, rw_socket, server_public, server_private):
         threading.Thread.__init__(self)
+        self.rw_socket = rw_socket
+        self.server_public_key = server_public
+        self.server_private = server_private
+
+        self.is_login = False
+        self.is_subscribed = False
+        self.is_block = False
 
     def run(self):
+        while True:
+            msg = self.rw_socket.recv(1024).decode()
+            ret = self.parser(msg)
+            self.rw_socket.send(ret.encode())
+
+    def check_in_list(self):
+        pass
+
+    def parser(self, received):
+        if received[0:3] == 'INF':
+            rest = received[3:].strip()
+            spl = rest.split(';')
+            if len(spl) == 5 and '' not in spl:
+                ctrl_uuid = spl[0]
+                ctrl_host = spl[1]
+                ctrl_port = spl[2]
+                ctrl_is_blogger = spl[3]
+                nick = spl[4]
+                client = Client(client_host=ctrl_host, client_port=ctrl_port, client_uuid=ctrl_uuid, server_private=self.server_private)
+                check = client.check_identity()
+                # TODO: IF check is OK add to table or do nothing
+                return check
+
+            else:
+                return 'REJ'
+
+
+    def response(self):
         pass
 
 
 class Client(threading.Thread):
-    def __init__(self, server_uuid, server_host, server_port, server_private ,host, port, cs_info, type):
+    def __init__(self, client_host, client_port, client_uuid, server_private, server_uuid=None, server_host=None, server_port=None):
         threading.Thread.__init__(self)
         # Blogger
         self.is_blogger = 'Y'
         # Its server information to share
-        self.server_uuid = server_uuid
-        self.server_host = server_host
-        self.server_port = server_port
-        self.server_private = server_private
+        self.server_uuid = server_uuid  # Request - For login protocol
+        self.server_host = server_host  # Request - For login protocol
+        self.server_port = server_port  # Request - For login protocol
+        self.server_private = server_private  # Request & Response - For decryption
         # TODO: Get nickname from interface
         self.nickname = ''
         # Other side server information to connect & check
-        self.host = host
-        self.port = port
+        self.client_host = client_host
+        self.client_port = client_port
+        self.client_uuid = client_uuid
         self.public_key = ''
-        # Client - Server Information Queue
-        self.cs_info = cs_info
+        # Client - Server Information Queue - Probably not necessary
+        # self.cs_info = cs_info
         # Socket
         self.sock = socket.socket()
         # False response count
         self.error = 0
-        # Request or Connect
-        self.type = type
+        # Request or Connect - Probably not necessary
+        # self.type = type
 
     def run(self):
         self.connect()
-        if self.type == 'Request':
-            pass
-        else:
-            pass
         # self.demand_public_key()
 
     # Request & Response
     def connect(self):
-        self.sock.connect((self.host, self.port))
+        self.sock.connect((self.client_host, self.client_port))
 
     # Request & Response
     def disconnect(self):
